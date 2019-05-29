801bbd412681cc954ff175ae1793d97cb1a68ae8
diegotbl <diegoblima2010@gmail.com>
2019-05-29 12:25:49 -0300

    removing unecessary lines and finishing password module

--- src/modcracklib.c
+++ src/modcracklib.c
@@ -1,35 +1,3 @@
-/*
-  * pam_cracklib module
-  */
-
- /*
-  * 0.9. switch to using a distance algorithm in similar()
-  * 0.86.  added support for setting minimum numbers of digits, uppers,
-  *        lowers, and others
-  * 0.85.  added six new options to use this with long passwords.
-  * 0.8. tidied output and improved D(()) usage for debugging.
-  * 0.7. added support for more obscure checks for new passwd.
-  * 0.6. root can reset user passwd to any values (it's only warned)
-  * 0.5. supports retries - 'retry=N' argument
-  * 0.4. added argument 'type=XXX' for 'New XXX password' prompt
-  * 0.3. Added argument 'debug'
-  * 0.2. new password is feeded to cracklib for verify after typed once
-  * 0.1. First release
-  */
-
- /*
-  * Modification for long password systems (>8 chars).  The original
-  * module had problems when used in a md5 password system in that it
-  * allowed too short passwords but required that at least half of the
-  * bytes in the new password did not appear in the old one.  this
-  * action is still the default and the changes should not break any
-  * current user. This modification adds 6 new options, one to set the
-  * number of bytes in the new password that are not in the old one,
-  * the other five to control the length checking, these are all
-  * documented (or will be before anyone else sees this code) in the PAM
-  * S.A.G. in the section on the cracklib module.
-*/
-
  #include <stdio.h>
  #ifdef HAVE_LIBXCRYPT
  #include <xcrypt.h>
@@ -47,8 +15,6 @@
  #include <limits.h>
  #include <pwd.h>
  #include <security/pam_modutil.h>
-
-#define CRACKLIB_DICTS NULL
 
  #ifdef MIN
  #undef MIN
@@ -71,30 +37,16 @@
 
  struct cracklib_options {
      int retry_times;
-     int diff_ok;
-     int min_length;
+     int length;
      int dig_credit;
-     int up_credit;
-     int low_credit;
-     int oth_credit;
-     int min_class;
-     int max_repeat;
-     int max_sequence;
-     int max_class_repeat;
      int reject_user;
-     int gecos_check;
      int enforce_for_root;
-     char *cracklib_dictpath;
  };
 
  #define CO_RETRY_TIMES  1
- #define CO_DIFF_OK      5
- #define CO_MIN_LENGTH   5
- #define CO_MIN_LENGTH_BASE 5
+ #define CO_LENGTH       5
+ #define CO_LENGTH_BASE  1
  #define CO_DIG_CREDIT   5
- #define CO_UP_CREDIT    0
- #define CO_LOW_CREDIT   0
- #define CO_OTH_CREDIT   0
  #define CO_MIN_WORD_LENGTH 5
 
 static int pam_parse(pam_handle_t *pamh, struct cracklib_options *opt,
@@ -114,68 +66,34 @@ int argc, const char **argv){
             opt->retry_times = strtol(*argv+6,&ep,10);
             if(!ep || (opt->retry_times < 1))
                 opt->retry_times = CO_RETRY_TIMES;
-        } else if(!strncmp(*argv,"difok=",6)) {
-            opt->diff_ok = strtol(*argv+6,&ep,10);
-            if(!ep || (opt->diff_ok < 0))
-                opt->diff_ok = CO_DIFF_OK;
         } else if (!strncmp(*argv,"difignore=",10)) {
             /* just ignore */
-        } else if (!strncmp(*argv,"minlen=",7)) {
-            opt->min_length = strtol(*argv+7,&ep,10);
-            if (!ep || (opt->min_length < CO_MIN_LENGTH_BASE))
-                opt->min_length = CO_MIN_LENGTH_BASE;
+        } else if (!strncmp(*argv,"len=",4)) {
+            opt->length = strtol(*argv+4,&ep,10);
+            if (!ep || (opt->length < CO_LENGTH_BASE))
+                opt->length = CO_LENGTH_BASE;
         } else if (!strncmp(*argv,"dcredit=",8)) {
-            opt->dig_credit = 5;
-        } else if (!strncmp(*argv,"ucredit=",8)) {
-            opt->up_credit = strtol(*argv+8,&ep,10);
+            opt->dig_credit = strtol(*argv+8,&ep,10);
             if (!ep)
-                opt->up_credit = 0;
-        } else if (!strncmp(*argv,"lcredit=",8)) {
-            opt->low_credit = strtol(*argv+8,&ep,10);
-            if (!ep)
-                opt->low_credit = 0;
-        } else if (!strncmp(*argv,"ocredit=",8)) {
-            opt->oth_credit = strtol(*argv+8,&ep,10);
-            if (!ep)
-                opt->oth_credit = 0;
-        } else if (!strncmp(*argv,"minclass=",9)) {
-            opt->min_class = strtol(*argv+9,&ep,10);
-            if (!ep)
-                opt->min_class = 0;
-            if(opt->min_class > 4)
-                opt->min_class = 4;
-            } else if (!strncmp(*argv,"maxrepeat=",10)) {
-                opt->max_repeat = strtol(*argv+10,&ep,10);
-                if (!ep)
-                    opt->max_repeat = 0;
-            } else if (!strncmp(*argv,"maxsequence=",12)) {
-                opt->max_sequence = strtol(*argv+12,&ep,10);
-                if (!ep)
-                    opt->max_sequence = 0;
-            } else if (!strncmp(*argv,"maxclassrepeat=",15)) {
-                opt->max_class_repeat = strtol(*argv+15,&ep,10);
-                if (!ep)
-                    opt->max_class_repeat = 0;
-            } else if (!strncmp(*argv,"reject_username",15)) {
-                opt->reject_user = 1;
-            } else if (!strncmp(*argv,"gecoscheck",10)) {
-                opt->gecos_check = 1;
-            } else if (!strncmp(*argv,"enforce_for_root",16)) {
-                opt->enforce_for_root = 1;
-            } else if (!strncmp(*argv,"authtok_type",12)) {
-                /* for pam_get_authtok, ignore */;
-            } else if (!strncmp(*argv,"use_authtok",11)) {
-                /* for pam_get_authtok, ignore */;
-            } else if (!strncmp(*argv,"use_first_pass",14)) {
-                /* for pam_get_authtok, ignore */;
-            } else if (!strncmp(*argv,"try_first_pass",14)) {
-                /* for pam_get_authtok, ignore */;
-            } else {
-                pam_syslog(pamh,LOG_ERR,"pam_parse: unknown option; %s",*argv);
-            }
-      }
+                opt->dig_credit = CO_DIG_CREDIT;
+        } else if (!strncmp(*argv,"reject_username",15)) {
+            opt->reject_user = 1;
+        } else if (!strncmp(*argv,"enforce_for_root",16)) {
+            opt->enforce_for_root = 1;
+        } else if (!strncmp(*argv,"authtok_type",12)) {
+            /* for pam_get_authtok, ignore */;
+        } else if (!strncmp(*argv,"use_authtok",11)) {
+            /* for pam_get_authtok, ignore */;
+        } else if (!strncmp(*argv,"use_first_pass",14)) {
+            /* for pam_get_authtok, ignore */;
+        } else if (!strncmp(*argv,"try_first_pass",14)) {
+            /* for pam_get_authtok, ignore */;
+        } else {
+            pam_syslog(pamh,LOG_ERR,"pam_parse: unknown option; %s",*argv);
+        }
+    }
 
-      return ctrl;
+    return ctrl;
 }
 
 /* Helper functions*/
@@ -183,20 +101,19 @@ char *password_check(pam_handle_t *pamh, struct cracklib_options *opt,
 const char *oldp, const char *newp, const char *user) {
     char *msg = NULL;
     int i = 0;
+    int number_digits = 0;
 
-    if(strlen(newp) != 5){
-        msg = "5 characters required";
-        pam_syslog(pamh, LOG_NOTICE, "mensagem com mais ou menos de 5 carac: %s", msg);
-    } else{
-        while(i < 5){
-            if(!isdigit(newp[i])){
-                msg = "only digits allowed";
-                pam_syslog(pamh, LOG_NOTICE, "%s", msg);
-            }
+    if(strlen(newp) != opt->length)
+        msg = "wrong number of characters";
+    else{
+        while(i < opt->length){
+            if(isdigit(newp[i]))
+                number_digits++;
             i++;
         }
+        if(number_digits != opt->dig_credit)
+            msg = "wrong amount of digits";
     }
-    pam_syslog(pamh, LOG_NOTICE, "sem mensagem: %s", msg);
 
     return msg;
 }
@@ -253,13 +170,8 @@ int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
 
     memset(&options, 0, sizeof(options));
     options.retry_times = CO_RETRY_TIMES;
-    options.diff_ok = CO_DIFF_OK;
-    options.min_length = CO_MIN_LENGTH;
+    options.length = CO_LENGTH;
     options.dig_credit = CO_DIG_CREDIT;
-    options.up_credit = CO_UP_CREDIT;
-    options.low_credit = CO_LOW_CREDIT;
-    options.oth_credit = CO_OTH_CREDIT;
-    options.cracklib_dictpath = CRACKLIB_DICTS;
 
     ctrl = pam_parse(pamh, &options, argc, argv);
 
@@ -319,18 +231,14 @@ int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
 
             retval = pam_get_authtok_verify(pamh, &newtoken, NULL);
             if (retval != PAM_SUCCESS) {
-                pam_syslog(pamh,LOG_NOTICE, "vovo juju");
                 pam_syslog(pamh, LOG_NOTICE, "pam_get_authtok_verify returned error: %s", pam_strerror (pamh, retval));
                 pam_set_item(pamh, PAM_AUTHTOK, NULL);
                 continue;
             } else if(newtoken == NULL) {      /* user aborted password change, quit */
-                pam_syslog(pamh, LOG_ERR, "gesonel");
                 return PAM_AUTHTOK_ERR;
             }
-            pam_syslog(pamh, LOG_ERR, "jorel");
             return PAM_SUCCESS;
         }
-        pam_syslog(pamh, LOG_NOTICE, "returning because maxtries reached");
 
         pam_set_item (pamh, PAM_AUTHTOK, NULL);
 
